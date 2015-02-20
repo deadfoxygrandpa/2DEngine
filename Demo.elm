fc7b17d2ecf9eb7d06a7a85da1397b971cc645f9
Alex Neslusan <deadfoxygrandpa@gmail.com>
2015-02-20 20:26:08 +0800

    Update demo

--- Demo.elm
+++ Demo.elm
@@ -1,15 +1,23 @@
 module Demo where
 
 import Color
+import Time (fps)
 import Signal ((<~))
+import Graphics.Element (color)
+import Debug
 
 import Engine
 
 texture = Engine.texture "font.png"
 
-scene t =
-    case t of
-        Just t' -> [Engine.texturedTile Color.purple t' (1, 0)]
-        Nothing -> []
+sprite t x y = Engine.spriteTile 261 55 26 44 Color.orange
+    t
+    (0, 0)
 
-main = Engine.display (5, 5) (64, 64) <~ (scene <~ texture)
+tex t _ _ = Engine.texturedTile Color.orange t (0, 0)
+
+scene t' = case t' of
+    Just t -> [sprite t 0 0]
+    Nothing -> []
+
+main = (\texture -> color Color.purple <| Engine.display (1, 1) (512, 512) (scene texture)) <~ texture
