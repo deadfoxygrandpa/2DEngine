83bb26e3bd5753b452840e2085e560686940cc23
Alex Neslusan <deadfoxygrandpa@gmail.com>
2015-02-22 15:52:00 +0800

    Make the Demo take type input

--- Demo.elm
+++ Demo.elm
@@ -2,13 +2,16 @@ module Demo where
 
 import Color
 import Time (fps)
-import Signal ((<~))
-import Graphics.Element (color)
-import Debug
+import Signal ((<~), (~))
+import Graphics.Element (color, flow, down, Element)
+import Graphics.Input as Input
+import Graphics.Input.Field as Field
+import Signal
 
 import Engine
+import BMFont
 
-texture = Engine.texture "font.png"
+texture = Engine.texture "font2_0.png"
 
 sprite t x y = Engine.spriteTile 261 55 26 44 Color.orange
     t
@@ -16,8 +19,19 @@ sprite t x y = Engine.spriteTile 261 55 26 44 Color.orange
 
 tex t _ _ = Engine.texturedTile Color.orange t (0, 0)
 
-scene t' = case t' of
-    Just t -> [sprite t 0 0]
+txt t s = BMFont.write s Color.red t (-25, 0)
+
+scene t' s = case t' of
+    Just t -> txt t s
     Nothing -> []
 
-main = (\texture -> color Color.purple <| Engine.display (1, 1) (512, 512) (scene texture)) <~ texture
+msg : Signal.Channel Field.Content
+msg = Signal.channel Field.noContent
+
+msgString : Signal.Signal String
+msgString = .string <~ Signal.subscribe msg
+
+msgField : Signal.Signal Element
+msgField = Field.field Field.defaultStyle (Signal.send msg) "type here" <~ Signal.subscribe msg
+
+main = (\texture field s -> flow down [color Color.purple <| Engine.display (50, 1) (15, 32) (scene texture s), field]) <~ texture ~ msgField ~ msgString
